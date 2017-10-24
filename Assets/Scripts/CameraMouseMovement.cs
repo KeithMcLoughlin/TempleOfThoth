1a132893c61c79fc6fbc54e09551493cab8b488d
Keith <kmcl1321@gmail.com>
2017-10-24 19:44:29 +0100

    player cursor changes colour when they can interact

--- Assets/Scripts/CameraMouseMovement.cs
+++ Assets/Scripts/CameraMouseMovement.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraMouseMovement : MonoBehaviour {
 
     public float sensitivity = 5f;
     public float smoothing = 2f;
     public float maxVerticalRotation = 90f; //in degrees
+    public float interactDistance = 2.5f;
+    public GameObject playerCursor;
 
     Vector2 lookingPosition;
     Vector2 smoothingVector;
     Vector2 mouseScalar;
     GameObject player;
+    Ray interactIndicator;
+    RaycastHit interactable;
+    int interactableLayer;
+    Image playerCursorImage;
 
 	// Use this for initialization
 	void Start ()
@@ -19,6 +26,8 @@ public class CameraMouseMovement : MonoBehaviour {
         player = this.transform.parent.gameObject;
         mouseScalar = new Vector2(sensitivity * smoothing, sensitivity * smoothing);
         Cursor.visible = false;
+        interactableLayer = LayerMask.GetMask("Interactable");
+        playerCursorImage = playerCursor.GetComponent<Image>();
     }
 	
 	// Update is called once per frame
@@ -34,5 +43,16 @@ public class CameraMouseMovement : MonoBehaviour {
 
         transform.localRotation = Quaternion.AngleAxis(-lookingPosition.y, Vector3.right);
         player.transform.rotation = Quaternion.AngleAxis(lookingPosition.x, player.transform.up);
+
+        interactIndicator.origin = this.transform.position;
+        interactIndicator.direction = this.transform.forward;
+        if(Physics.Raycast(interactIndicator, out interactable, interactDistance, interactableLayer))
+        {
+            playerCursorImage.color = Color.green;
+        }
+        else
+        {
+            playerCursorImage.color = Color.red;
+        }
     }
 }
--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonClicked : MonoBehaviour {
+public class Interact : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +13,9 @@ public class ButtonClicked : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    void Action()
+    {
+
+    }
 }
