8e411fd97e070dbdefd89d2cc24f61564cbcc866
Keith <kmcl1321@gmail.com>
2017-11-12 19:55:34 +0000

    added enemy that chases the player

---
+++ Assets/Scripts/AI/ChasingEnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChasingEnemyMovement : MonoBehaviour {
+
+    PlayerController player;
+    NavMeshAgent navMesh;
+
+	void Start ()
+    {
+        player = PlayerController.Instance;
+        navMesh = GetComponent<NavMeshAgent>();
+	}
+	
+	void Update ()
+    {
+        //when the player enters the nav mesh, start chasing them
+        var path = new NavMeshPath();
+        if(navMesh.CalculatePath(player.transform.position, path))
+        {
+            navMesh.SetPath(path);
+        }
+    }
+}
